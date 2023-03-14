a40d62f933293fe13e8e8be3ef3ebfd0282395d7
JakubKivi <jakub.kivi.kuziemski@gmail.com>
2023-03-14 23:07:56 +0100

    working on cool typing mechanism

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function squareBlink() {
+    var square =  document.getElementById("squareBlink")
+    var text =  document.getElementById("second-line-text")
+    if(square.classList.contains("square-blink-off")){
+        square.classList.remove("square-blink-off");
+        text.classList.remove("text-positioning");
+    }else{
+        square.classList.add("square-blink-off");
+        text.classList.add("text-positioning");
+    }
+}
+
+window.setInterval(function(){
+    squareBlink();
+}, 1000);
+
+
+var  iTyping = 0;
+var jTyping=0;
+var txt = ['Programmer.', 'Robocisit.','Electronic engineer.', 'Traveller.'];
+var speed = 200;
+let delay = 5;
+var backspace = false;
+
+var text = document.getElementById("second-line-text");
+
+async function typing() {
+  text.textContent = txt[jTyping].substring(0, iTyping);
+
+  if ( iTyping > txt[jTyping].length + delay){
+    await new Promise(r => setTimeout(r, 3000));
+    backspace = true;
+  } 
+  if ( iTyping == -1) backspace = false;
+  iTyping =  iTyping + (backspace ? -1 : 1);
+  if( iTyping != -1) setTimeout(typing, speed);
+  else{
+    await new Promise(r => setTimeout(r, 2000));
+    jTyping+=1;
+    if(jTyping >= txt.length) jTyping=0;
+    typing();
+  } 
+}
+
+typing();
+
