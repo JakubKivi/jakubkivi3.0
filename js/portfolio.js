efe30183b46d84c137c0a51817c39005485cd460
JakubKivi <jakub.kivi.kuziemski@gmail.com>
2023-03-22 01:21:33 +0100

    portfolio selector v1 is working

---
+++ js/portfolio.js
@@ -0,0 +1,38 @@
+var selector = document.getElementsByClassName("portfolio-selector")[0].children;
+
+var activeSelector=1;
+
+for (var i = 0; i < selector.length; i++) { 
+    selector[i].addEventListener("click", onSelectorClicked);
+}
+
+async function onSelectorClicked(element){
+    var id = element.currentTarget.getAttribute("data-userid");
+    if(id!=activeSelector){
+        selector[activeSelector-1].children[0].classList.remove("active");
+        selector[id-1].children[0].classList.add("active");
+        activeSelector = id;
+        var rightColumn = document.getElementById("right-column");
+        rightColumn.style.opacity = 0;
+        
+        await new Promise(r => setTimeout(r, 500));
+
+        ChangePortfolioContent(id);
+    }
+}
+
+function ChangePortfolioContent(a){
+    var myModal = document.getElementById("modal-"+a);
+    
+    document.getElementById("portfolio-text").innerHTML = 
+    myModal.getElementsByClassName("portfolio-text")[0].innerHTML;
+    
+    document.getElementById("portfolio-image").innerHTML = 
+    myModal.getElementsByClassName("img-container")[0].innerHTML;
+
+    document.getElementById("portfolio-title").innerHTML = 
+    myModal.getElementsByClassName("modal-title")[0].innerHTML;
+
+    var rightColumn = document.getElementById("right-column");
+    rightColumn.style.opacity = 1;
+}
