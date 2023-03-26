cef2182fa4c7e0b7ab7ed62ad795409ec873029c
JakubKivi <jakub.kivi.kuziemski@gmail.com>
2023-03-26 03:43:35 +0200

    started about section, finished prtfolio

---
+++ js/onLoad.js
@@ -0,0 +1,4 @@
+onLoad();
+function onLoad(){
+    onSelectorClicked();
+}
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,31 +1,46 @@
-var selector = document.getElementsByClassName("portfolio-selector")[0].children;
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
-var activeSelector=1;
+var selector = document.getElementsByClassName("portfolio-selector")[0].children;
+var activeSelector=2;
 
 for (var i = 0; i < selector.length; i++) { 
     selector[i].addEventListener("click", onSelectorClicked);
 }
 
 async function onSelectorClicked(element){
-    var id = element.currentTarget.getAttribute("data-userid");
-    if(id!=activeSelector){
-        selector[activeSelector-1].children[0].classList.remove("active");
-        selector[id-1].children[0].classList.add("active");
-        activeSelector = id;
-        var rightColumn = document.getElementById("right-column");
-        rightColumn.style.opacity = 0;
-        
-        await new Promise(r => setTimeout(r, 500));
+    if(element!=null){
+        var id = element.currentTarget.getAttribute("data-userid");
+    }else{
+        var id = "1";
+        var firstLoad =true;
+    } 
+    if(width>769){
+        if(id!=activeSelector){
+            selector[activeSelector-1].children[0].classList.remove("active");
+            selector[id-1].children[0].classList.add("active");
+            activeSelector = id;
+            var rightColumn = document.getElementById("right-column");
+            rightColumn.style.opacity = 0;
+            
+            await new Promise(r => setTimeout(r, 500));
 
-        ChangePortfolioContent(id, rightColumn);
+            ChangePortfolioContent(id, rightColumn);
+        }
+    }else if(!firstLoad){
+        console.log("openpopup");
+        var modalName = '#portfolio-modal-'+id;
+        $(modalName).modal();
     }
+    
 }
 
 function ChangePortfolioContent(a, r){
     var myModal = document.getElementById("modal-"+a);
-    
+    console.log(width);
     document.getElementById("portfolio-text").innerHTML = 
-    myModal.getElementsByClassName("portfolio-text")[0].innerHTML;
+    myModal.getElementsByClassName("portfolio-text")[0].innerHTML.slice(0, 900 *width/1536) + "... " +
+    "<a href=\"\" data-toggle=\"modal\" data-target=\"#portfolio-modal-"+a+"\">Read more. </a>";
+    //width/1536 is screen width coefficient to trim text more on smaller screens ;)
     
     document.getElementById("portfolio-image").innerHTML = 
     myModal.getElementsByClassName("img-container")[0].innerHTML;
