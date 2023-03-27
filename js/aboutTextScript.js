8903b40d369098df28059c1c0024a95a5b0706dd
JakubKivi <jakub.kivi.kuziemski@gmail.com>
2023-03-27 02:00:59 +0200

    about v1.1

--- js/aboutTextScript.js
+++ js/aboutTextScript.js
@@ -1,10 +1,10 @@
 
 var showPage=0;
-var aboutContainer = document.getElementById("about-text");
+var aboutContainer = document.getElementById("about-left");
 
 async function aboutTextInsert(){
     aboutContainer.classList.add("hiding");
-    await new Promise(r => setTimeout(r, 500));
+    await new Promise(r => setTimeout(r, 800));
     document.getElementById("about-text").innerHTML= aboutText[showPage];
     aboutContainer.classList.remove("hiding");
 } 
